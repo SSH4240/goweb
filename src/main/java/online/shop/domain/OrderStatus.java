7a1ac56836e977684fb387d44d6042f540279159
SSH <pdxucxy@gmail.com>
2023-04-27 14:52:04 +0900

    0427 study

--- src/main/java/online/shop/domain/OrderStatus.java
+++ src/main/java/online/shop/domain/OrderStatus.java
@@ -2,5 +2,5 @@ package online.shop.domain;
 
 public enum OrderStatus {
     ORDER,
-    CANCELED
+    CANCEL
 }
--- src/main/java/online/shop/domain/item/Item.java
+++ src/main/java/online/shop/domain/item/Item.java
@@ -3,6 +3,7 @@ package online.shop.domain.item;
 import lombok.Getter;
 import lombok.Setter;
 import online.shop.domain.Category;
+import online.shop.exception.NotEnoughStockException;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -23,4 +24,22 @@ public abstract class Item {
 
     @ManyToMany(mappedBy="items")
     private List<Category> categories = new ArrayList<Category>();
+    //==비즈니스 로직==//
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
